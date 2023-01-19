91ff92eb907ae15ccb792ab3a87ff367c37104a0
Hairui Lin <hairuilin@google.com>
2023-01-19 15:40:30 +0800

    add column annotations to User entity

--- src/main/java/com/hairui/reveal/users/entities/User.java
+++ src/main/java/com/hairui/reveal/users/entities/User.java
@@ -1,24 +1,38 @@
 package com.hairui.reveal.users.entities;
 
-import com.hairui.reveal.users.UserRole;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import java.util.Set;
 
 @Entity
 public class User {
 
   @Id
+  @Column(name = "email")
   private String email;
 
+  @Column(name = "first_name")
   private String firstName;
 
+  @Column(name = "last_name")
   private String lastName;
 
+  @Column(name = "password")
   private String password;
 
+  @Column(name = "is_enabled")
   private boolean isEnabled;
 
-  private Set<UserRole> roles;
+  @ElementCollection
+  @CollectionTable(
+      name = "UserRoles",
+      joinColumns = @JoinColumn(name = "email")
+  )
+  @Column(name = "roles")
+  private Set<String> roles;
 
 }

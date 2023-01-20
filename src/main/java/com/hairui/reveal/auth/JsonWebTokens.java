d02aeb15fa5b755a3b8e21fa3e7756d8638bc8a4
Hairui Lin <hairuilin@google.com>
2023-01-20 08:23:19 +0800

    refactored JsonWebTokens class to have more appropriate method definitions

--- src/main/java/com/hairui/reveal/auth/JsonWebTokens.java
+++ src/main/java/com/hairui/reveal/auth/JsonWebTokens.java
@@ -7,46 +7,55 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
-public final class JwtService {
+/** A utility class for parsing and generating json web tokens. */
+@Component
+public final class JsonWebTokens {
 
   private static final String SECRET_KEY = "337436763979244226452948404D635166546A576E5A7234753778217A25432A";
 
-  public Optional<String> getUserEmail(String token) {
+  /** Parse an input json web token and extra the email address of the user. */
+  public static Optional<String> getUserEmail(String token) {
     // TODO: revisit here after the encoding is done to see how we can have a better naming
     return extractClaim(token, Claims::getSubject);
   }
 
-  public String generateToken(
-      Map<String, Object> extraClaims,
+  /** Generate a json web token using the input user details. */
+  public static String generateToken(
       UserDetails userDetails
   ) {
+    Map<String, Object> extraClaims = new HashMap<>();
+    return createJsonWebToken(extraClaims, userDetails);
+  }
+
+  private static String createJsonWebToken(Map<String, Object> extraClaims, UserDetails userDetails) {
     return Jwts.builder()
         .setClaims(extraClaims)
         .setSubject(userDetails.getUsername())
         .setIssuedAt(new Date(System.currentTimeMillis()))
         .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-        .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+        .signWith(getSigningKey(), SignatureAlgorithm.ES256)
         .compact();
   }
 
-  public <T> Optional<T> extractClaim(String token, Function<Claims, T> claimsResolver) {
+  private static <T> Optional<T> extractClaim(String token, Function<Claims, T> claimsResolver) {
     return extractAllClaims(token).map(claimsResolver::apply);
   }
 
-  private Optional<Claims> extractAllClaims(String token) {
-    return Optional.of(Jwts.parserBuilder().setSigningKey(getSignInKey()).build()
+  private static Optional<Claims> extractAllClaims(String token) {
+    return Optional.of(Jwts.parserBuilder().setSigningKey(getSigningKey()).build()
         .parseClaimsJws(token)
         .getBody());
   }
 
-  private Key getSignInKey() {
+  private static Key getSigningKey() {
     byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
     return Keys.hmacShaKeyFor(keyBytes);
   }

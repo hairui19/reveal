1834e49e6995452f882b6385cdf52467ca65ff72
Hairui Lin <hairuilin@google.com>
2023-01-20 15:27:21 +0800

    add SecurityConfiguration class

--- src/main/java/com/hairui/reveal/config/SecurityConfiguration.java
+++ src/main/java/com/hairui/reveal/config/SecurityConfiguration.java
@@ -1,2 +1,17 @@
-package com.hairui.reveal.config;public class SecurityConfiguration {
+package com.hairui.reveal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public final class SecurityConfiguration {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity.build();
+  }
 }
